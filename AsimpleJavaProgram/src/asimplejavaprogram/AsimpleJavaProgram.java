5a5abb616f38a5408d7b1b08a5ef9fc52fb6c560
tamzi <tamrefrank@gmail.com>
2015-10-13 16:06:48 +0300

    a simple java prog

---
+++ AsimpleJavaProgram/src/asimplejavaprogram/AsimpleJavaProgram.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asimplejavaprogram;
+
+/**
+ *
+ * @author tamreFrank
+ */
+public class AsimpleJavaProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       System.out.println("My first Java program.");
+       System.out.println("The sum of 2 and 3 = "+5);
+       System.out.println("7+8 = " + (7 + 8));// TODO code application logic here
+    }
+}
